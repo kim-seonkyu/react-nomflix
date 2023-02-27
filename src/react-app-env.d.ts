e7bf1f9f3f22b5de4ea94bcbc2bf2b8961f463d6
kim-seonkyu <seonkyu93@gmail.com>
2023-02-27 17:35:47 +0900

    fixed

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,6 +3,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
     PUBLIC_URL: string;
-    REACT_APP_REACT_APP_API_KEY: string;
+    REACT_APP_API_KEY: string;
   }
 }
